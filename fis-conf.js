047d3ed98a2888c67b6ae7344a57c046b46129c2
aaaaa <923345641@qq.com>
2019-10-14 16:06:22 +0800

    删除fis配置文件

--- fis-conf.js
+++
@@ -1,9 +0,0 @@
-const HOST = 'http://yq01-sunbai.epc.baidu.com:8200';
-const ROOT = '/home/work';
-
-fis.match('*', {
-    deploy: fis.plugin('http-push', {
-        host: HOST,
-        to: ROOT
-    })
-});
